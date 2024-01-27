2a0b66b137f6ccd4885069c8de7fc6414d093f7d
DiMaxTechnologies <157880947+DiMaxTechnologies@users.noreply.github.com>
2024-01-27 14:22:35 +0500

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "randomizeBetweenTwoNumbers.h"
+
+using namespace std;
+
+int main() {
+  cout << "Randomized number: " << randomizeBetween(9, 25) << endl;
+  return 0;
+}
